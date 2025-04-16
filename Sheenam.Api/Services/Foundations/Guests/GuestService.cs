506ada9822f49be2d2da77b9d34c2dcd4fd7176a
00017005 <00017005wiut@gmail.com>
2025-04-16 14:54:27 +0500

    CODERUB: Add Logging Broker

--- Sheenam.Api/Services/Foundations/Guests/GuestService.cs
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.cs
@@ -1,6 +1,8 @@
-﻿using Sheenam.Api.Brokers.Storages;
+﻿using Sheenam.Api.Brokers.Loggings;
+using Sheenam.Api.Brokers.Storages;
 using Sheenam.Api.Models.Foundations.Guests;
 using Sheenam.Api.Services.Foundations.Guests.Exceptions;
+using System;
 using System.Threading.Tasks;
 
 namespace Sheenam.Api.Services.Foundations.Guests
@@ -8,17 +10,23 @@ namespace Sheenam.Api.Services.Foundations.Guests
     public class GuestService : IGuestService
     {
         private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
 
-        public GuestService(IStorageBroker storageBroker) =>
+        public GuestService(IStorageBroker storageBroker, ILoggingBroker loggingBroker)
+        {
             this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
 
         public async ValueTask<Guest> AddGuestAsync(Guest guest)
         {
-            if (guest is null)
-            {
-                throw new GuestValidationException(new NullGuestException());
-            }
-            return await this.storageBroker.InsertGuestAsync(guest);
+            //this.loggingBroker.LogError(new Exception("something"));
+
+            //if (guest is null)
+            //{
+            //    throw new GuestValidationException(new NullGuestException());
+            //}
+            return await this.storageBroker.InsertGuestAsync(guest);    
         }
     }
 }
