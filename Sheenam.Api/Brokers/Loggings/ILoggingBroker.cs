a9879f99c198fd44e2992b5ea38d3c4c534c25bc
00017005 <00017005wiut@gmail.com>
2025-04-16 11:59:04 +0500

    BROKERS: Implement Logging

--- Sheenam.Api/Brokers/Loggings/ILoggingBroker.cs
+++ Sheenam.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using Microsoft.Extensions.Logging;
 
 namespace Sheenam.Api.Brokers.Loggings
 {
     public interface ILoggingBroker
     {
-        void LogError(Exception exception); 
+        void LogError(Exception exception);
         void LogCritical(Exception exception);
     }
 }
--- Sheenam.Api/Brokers/Loggings/LoggingBroker.cs
+++ Sheenam.Api/Brokers/Loggings/LoggingBroker.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
+using System;
 
 namespace Sheenam.Api.Brokers.Loggings
 {
     public class LoggingBroker : ILoggingBroker
     {
-		private readonly ILogger<LoggingBroker> logger;
+        private readonly ILogger<LoggingBroker> logger;
 
-		public LoggingBroker(ILogger<LoggingBroker> logger) =>
-			this.logger = logger;
+        public LoggingBroker(ILogger<LoggingBroker> logger) =>
+            this.logger = logger;
 
-		public void LogError(Exception exception) =>
-			this.logger.LogError(exception, exception.Message)
+        public void LogError(Exception exception) =>
+            this.logger.LogError(exception, exception.Message);
 
         public void LogCritical(Exception exception) =>
-			this.logger.LogCritical(exception, exception.Message)
+            this.logger.LogCritical(exception, exception.Message);
     }
 }
